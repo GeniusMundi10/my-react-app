044ceb3a69e1bc212fc22da718d716d9b6f4b815
GeniusMundi10 <159649324+GeniusMundi10@users.noreply.github.com>
2024-08-29 21:23:06 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is my React app hosted on GitHub Pages!</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
